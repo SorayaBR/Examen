dfb5ce0d3de440da76d001efd89645bbfd84a79a
SorayaBR <soraya.bellmont@estudiants.urv.cat>
2023-11-17 09:19:54 +0100

    parte3

--- api.js
+++ api.js
@@ -1,22 +1,36 @@
 const HPAPI_URL='https://wizard-world-api.herokuapp.com';
-window.onload = async()=>{
-    const characters= await getAllHPCharacters();
-    const spinnerHtmlElement = document.getElementById('spinner');
+
+window.onload=async()=>{
+    const wizards=await getAllWizards();
+
+    const spinnerHtmlElement  = document.getElementById('spinner');
     spinnerHtmlElement.remove();
-    for(const character of characters){
-        const mainHtmlElement=document.getElementById('main');
+
+    for(const wizard of wizards){
+        const mainHtmlElement = document.getElementById('main');
         const newElement = document.createElement('div');
-        newElement.innerHTML=`<h2${character.lastname}</h2>`;
+        newElement.innerHTML=`<h2>${wizard.firstName}</h2>
+        `
         mainHtmlElement.appendChild(newElement);
     }
+    const elixirs= await getAllElixirs();
+    for(const elixir of elixirs){
+        const mainHtmlElement2 = document.getElementById('main');
+        const newElement2 = document.createElement('div');
+        newElement2.innerHTML=`<p>${elixir.name}</p>
+        `
+        mainHtmlElement2.appendChild(newElement2);
+    }
+    
 };
-async function getAllHPCharacters(){
-    const response = await fetch(`${HPAPI_URL}/wizards`);
-    const data=await response.json();
-    return data.results;
-}
-async function getAll(){
-    const response = await fetch(`${POKEAPI_URL}/Elixirs`);
+async function getAllWizards(){
+    const response = await fetch(`${HPAPI_URL}/Wizards`);
     const data = await response.json();
-    return data.results;
+    debugger;
+    return data;
+}
+async function getAllElixirs(){
+    const response = await fetch(`${HPAPI_URL}/Elixirs`);
+    const data = await response.json();
+    return data;
 }
